3940a5a3496a422583967872009c66cb0a2865d4
CratosPool <luisjivillalba@gmail.com>
2020-07-07 15:38:46 -0300

    asd

--- Udemy/Angular/02-spa/src/app/components/heroes/heroes.component.ts
+++ Udemy/Angular/02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -7,14 +9,20 @@ import { HeroesService, Heroe } from '../../services/heroes.service';
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
-  heroes:Heroe[] = [];
-  constructor( private _heroesService: HeroesService ){
+  heroes: Heroe[] = [];
+  constructor( private _heroesService: HeroesService,
+               private router: Router
+             ){
     console.log('Constructor');
    }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes)
+    console.log(this.heroes);
+  }
+
+  verHeroe(i: number){
+    this.router.navigate( ['/heroe', i] );
   }
 
 }
--- Udemy/Angular/02-spa/src/app/services/heroes.service.ts
+++ Udemy/Angular/02-spa/src/app/services/heroes.service.ts
@@ -59,8 +59,24 @@ export class HeroesService {
         console.log('servicio listo para usar!!!!');
     }
 
-    getHeroes():Heroe[]{
+    getHeroes(): Heroe[]{
         return this.heroes;
+    }
+
+    getHeroe(i: string): Heroe{
+      return this.heroes[i];
+    }
+
+    buscarHeores(termino: string): Heroe[]{
+      let heroesArr: Heroe[] = [];
+      termino = termino.toLowerCase();
+      for (let heroe of this.heroes){
+        let nombre = heroe.nombre.toLowerCase();
+        if (nombre.indexOf( termino ) >= 0){
+          heroesArr.push( heroe );
+        }
+      }
+      return heroesArr;
     }
 
 }
