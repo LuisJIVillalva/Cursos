0f570b101d7dfadc1943f4ff9b1397d4040ff886
CratosPool <luisjivillalba@gmail.com>
2020-07-07 18:01:02 -0300

    se arreglaron unos errores en app.routes.ts

---
+++ Udemy/Angular/02-spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styles: [
+  ]
+})
+export class BuscadorComponent implements OnInit {
+
+  constructor( private activatedRoute: ActivatedRoute ) {
+
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( params =>{
+      console.log(params['temino'])
+    } )
+  }
+
+}
