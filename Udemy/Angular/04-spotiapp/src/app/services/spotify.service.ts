f78a147444d202ce2f1468097593a8877b7500cc
CratosPool <luisjivillalba@gmail.com>
2020-07-13 23:27:40 -0300

    va queriendo

--- Udemy/Angular/04-spotiapp/src/app/services/spotify.service.ts
+++ Udemy/Angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +9,29 @@ import { HttpClient } from '@angular/common/http';
 export class SpotifyService {
 
   constructor( private http: HttpClient) {
-    console.log('asd');
+  }
+
+  getQuery( query: string){
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      'Authorization' : 'Bearer BQDtG7Lceyor3AtP84r6ekoBRHuIv0-BJor75zfSg3UH9sJeTXAQe_Qyb3BE2BV8zOP-6L86KIMN0WbcFxo'
+    });
+
+    return this.http.get(url, { headers });
   }
 
   getNewReleases(){
-  this.http.get('https://api.spotify.com/v1/browse/new-releases')
-  .subscribe( data => {
-    console.log(data);
-  })
+    return this.getQuery('browse/new-releases')
+    .pipe( map( data => data['albums'].items));
   }
+
+  getArtista(termino: string){
+   
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+    .pipe( map( data => data['artists'].items));
+
+  }
+
 }
